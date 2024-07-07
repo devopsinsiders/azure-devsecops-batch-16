58d8dfdeac87559afd8294b6d652a99c9f6fc3e6
bot <info@devopsinsiders.com>
2024-07-07 20:54:26 +0530

    updated code

--- CodeSamples/TerraformBackendSetup/main.tf
+++ CodeSamples/TerraformBackendSetup/main.tf
@@ -15,14 +15,13 @@ terraform {
 
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.111.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-dhondhu-1"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "satfdevwesteurope01"   # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "devstatefile"          # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
-
 provider "azurerm" {
   features {}
 }
