60f5ebc921050151f9ca5b0ee0a1c596f6587309
bot <info@devopsinsiders.com>
2024-07-07 20:54:10 +0530

    updated code

--- CodeSamples/TerraformBackendSetup/main.tf
+++ CodeSamples/TerraformBackendSetup/main.tf
@@ -13,6 +13,16 @@ terraform {
   }
 }
 
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.111.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
