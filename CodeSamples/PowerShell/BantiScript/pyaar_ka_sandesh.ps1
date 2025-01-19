c5d9728af3b5fbc7d3750b3c9b23dbed21b8cf61
bot <info@devopsinsiders.com>
2025-01-19 23:31:29 +0530

    Added notes

---
+++ CodeSamples/PowerShell/BantiScript/pyaar_ka_sandesh.ps1
@@ -0,0 +1,3 @@
+New-Item -Path "C:\" -Name "PYAAR_KA_SANDESH" -ItemType Directory
+New-Item -Path "C:\PYAAR_KA_SANDESH" -Name "LoveLetter.txt" -ItemType File
+Set-Content -Path "C:\PYAAR_KA_SANDESH\LoveLetter.txt" -Value "I love you!"
---
+++ CodeSamples/PowerShell/RG/terraform.tfvars
@@ -0,0 +1,10 @@
+rgs = {
+    rg1 = {
+        name = "rg1"
+        location = "eastus"
+    }
+    rg2 = {
+        name = "rg2"
+        location = "westus"
+    }
+}
